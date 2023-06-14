bb77a090ec2cb4064cf56aa0252a56f4762a7208
Can Gumus <mzyt.gms@gmail.com>
2023-06-14 13:33:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Destek ve Şikayet Platformu',
+      title: 'Campus Alive',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
